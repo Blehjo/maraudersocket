c5803a78842fee5cfc3f49a64bf6a153997a4ea1
Blehjo <blehjooo@gmail.com>
2023-10-12 16:18:37 -0500

    changed url

--- Api/pin.api.ts
+++ Api/pin.api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Pin } from '../Types/pin.type';
 
-const api = "https://localhost:7144/api/pins";
+const api = "https://marauderserver.azurewebsites.net/api/pins";
 
 const headers = {
     'Accept': 'application/x-www-form-urlencoded',
